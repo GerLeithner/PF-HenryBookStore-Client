7863e034125707fde0cf8e6620bea9ef0b80bcc8
gerleithner <ger.leithner@gmail.com>
2022-12-18 01:55:21 -0300

    fixing NavBar component

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,20 +1,16 @@
 import React from "react";
-import { ContainerNavBar, LinkNavBar, Wrapper } from "../styles/NavBar";
+import { ContainerNavBar, SubContainerNavBar, LinkNavBar, HomeLinkNavBar } from "../styles/NavBar";
 
 const NavBar = () => {
   return (
     <ContainerNavBar>
-      <Wrapper>
-        <LinkNavBar to={"/home"}>Home</LinkNavBar>
-        <LinkNavBar to={"/catalogue"}>Complete Catalogue</LinkNavBar>
-        <LinkNavBar to={"/create"}>Add a new Book</LinkNavBar>
-        <LinkNavBar to={"/AboutUs"}>About Us</LinkNavBar>
+        <HomeLinkNavBar to={"/home"}>Books Explorer</HomeLinkNavBar>
+        <SubContainerNavBar>
+          <LinkNavBar to={"/catalogue"}>Catalogue</LinkNavBar>
+          <LinkNavBar to={"/create"}>New Book</LinkNavBar>
+          <LinkNavBar to={"/AboutUs"}>About Us</LinkNavBar>
+        </SubContainerNavBar>
 
-        {/* <div>
-            <SearchBar
-            paginado={paginado}/>
-           </div> */}
-      </Wrapper>
     </ContainerNavBar>
   );
 };
--- src/styles/NavBar.js
+++ src/styles/NavBar.js
@@ -2,38 +2,59 @@ import styled from "styled-components";
 import { Link } from "react-router-dom";
 
 export const ContainerNavBar = styled.div`
+  z-index: 1;
+  width: 100%;
+  padding: 0px 30px 0px 30px;
+  height: 50px;
   background-color: green;
-  width: 100%;
-  height: 70px;
-`;
-
-export const Wrapper = styled.div`
-  width: 100%;
-  max-width: 1300px;
-  height: 100%;
   display: flex;
-  flex-wrap: wrap;
-  justify-content: flex-end;
-  margin: auto;
-  position: sticky;
-`;
-
-export const LinkNavBar = styled(Link)`
-  display: flex;
-  text-decoration: none;
-  font-family: "Roboto Condensed", sans-serif;
-  font-weight: bold;
+  flex-direction: row;
+  justify-content: space-between;   
   align-items: center;
-  text-decoration: none;
-  color: white;
-  font-size: 1rem;
-  line-height: 1.75rem;
-  padding: 0.8rem;
-  position: relative;
+`;
+
+export const SubContainerNavBar = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between; 
+  gap: 70px;
+  align-items: center;
+`;
+
+export const HomeLinkNavBar = styled(Link)`
+  display: flex;
+  align-items: center;
   transition: background-color 0.5s ease-in-out;
+  color: white;
+  text-Decoration: none;
+  font-weight: 600;
+  font-size: 25px;
+  font-style: italic;
+  height: 50px;
+  padding: 0px 15px 0px 15px;
 
   &:hover {
     color: #ffffff;
     background-color: #5ca91d;
   }
 `;
+
+export const LinkNavBar = styled(Link)`
+  display: flex;
+  align-items: center;
+  transition: background-color 0.5s ease-in-out;
+  color: white;
+  text-Decoration: none;
+  font-weight: 400;
+  font-size: 20px;
+  height: 50px;
+  padding: 0px 15px 0px 15px;
+
+  &:hover {
+    color: #ffffff;
+    background-color: #5ca91d;
+  }
+`;
+
+
+
