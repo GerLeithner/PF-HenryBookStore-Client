45ac052fd8e270ea79fe54063bde56a6a1be3d4a
Adrianl93 <[200~laborde.adrian2012@gmail.comgit config --global user.email laborde.adrian2012@gmail.com>
2022-12-20 16:45:47 -0300

    options added

--- src/components/CardDetail.jsx
+++ src/components/CardDetail.jsx
@@ -20,6 +20,7 @@ import {
   H4Detail,
   ImgAndInfo,
   ButtonOptionsDetail,
+  ButtonSelectDetail,
 } from "../styles/Detail";
 import {ButtonCatalogue} from "../styles/Catalogue"
 
@@ -98,11 +99,16 @@ export default function CardDetail({book,modal,setModal}) {
     <ButtonCloseDetail onClick={e=>{handleCloseClick(e)}}><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-x" viewBox="0 0 16 16">
   <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z"/>
 </svg></ButtonCloseDetail>
-    <ButtonOptionsDetail>
-    <select>
+    <ButtonSelectDetail>
+      <ButtonOptionsDetail value="" hidden>Options</ButtonOptionsDetail>
+      <ButtonOptionsDetail value="review">Review</ButtonOptionsDetail>
+      <ButtonOptionsDetail value="readed">Readed</ButtonOptionsDetail>
+      <ButtonOptionsDetail value="favorite">Favorite</ButtonOptionsDetail>
     
-</select>
-</ButtonOptionsDetail>
+</ButtonSelectDetail>
+
+
+
 
         <TitleAndRating>
           <H1Detail>{book.title}</H1Detail>
