7c7742af19dc4c412cfe92e70412694436075512
Gabo <basiliogabrielalberto@gmail.com>
2022-12-28 16:52:55 -0300

    ProtectedRoutes

--- src/App.js
+++ src/App.js
@@ -7,19 +7,31 @@ import Catalogue from "./components/Catalogue.jsx";
 import AboutUs from "./components/AboutUs.jsx";
 import CreateBook from "./components/CreateBook.jsx";
 import NavBar from "./components/NavBar.jsx";
+import ProtectedRoute from "./auth/protected-route";
 
 function App() {
   return (
     <div className="App">
-        <Route exact path="/" component={ LandingPage } />
-        <Route path={["/home", "/home/:id", "/catalogue", "/create", "/about"]} component={ NavBar } /> 
-        <Route exact path="/home" component={ Home } />
-        <Route path="/home/:id" component={ CardDetail } />
-        <Route exact path="/catalogue" component={ Catalogue}/>
-        <Route exact path="/about" component={ AboutUs}/>
-        <Route exact path="/create" component={ CreateBook }/>
+      <Route exact path="/" component={LandingPage} />
+      <Route
+        path={["/home", "/home/:id", "/catalogue", "/create", "/about"]}
+        component={NavBar}
+      />
+      <Route exact path="/home" component={Home} />
+      <Route path="/home/:id" component={CardDetail} />
+      <Route exact path="/catalogue" component={Catalogue} />
+      <Route exact path="/about" component={AboutUs} />
+      <Route exact path="/create" component={CreateBook} />
     </div>
   );
 }
 
 export default App;
+
+/* {
+  <ProtectedRoute exact path="/home" component={Home} />
+      <ProtectedRoute path="/home/:id" component={CardDetail} />
+      <ProtectedRoute exact path="/catalogue" component={Catalogue} />
+      <ProtectedRoute exact path="/about" component={AboutUs} />
+      <ProtectedRoute exact path="/create" component={CreateBook} />
+} */
--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -4,7 +4,19 @@ import React from "react";
 export const Login = () => {
   const { loginWithRedirect } = useAuth0();
 
-  return <button onClick={() => loginWithRedirect()}>Login</button>;
+  return (
+    <button
+      onClick={async () =>
+        await loginWithRedirect({
+          authorizationParams: {
+            redirect_uri: "http://localhost:3000/home",
+          },
+        })
+      }
+    >
+      Login
+    </button>
+  );
 };
 
 /* import React from "react";
