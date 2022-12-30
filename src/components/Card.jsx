84119551154b4f5355da7689525e70f1f1b93d5d
Adrianl93 <[200~laborde.adrian2012@gmail.comgit config --global user.email laborde.adrian2012@gmail.com>
2022-12-30 11:27:01 -0300

    buttons fixed

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,11 +1,21 @@
 import React, { useState } from "react";
-import { bookDetail } from "../redux/actions";
+
+import {
+  bookDetail,
+  addFavorite,
+  addReaded,
+  addReading,
+  deleteFavorite,
+  deleteReading,
+  deleteReaded
+} from "../redux/actions";
 import { useDispatch, useSelector } from "react-redux";
 import CardDetail from "./CardDetail.jsx";
 import caretIcon from "../icons/caretIcon.svg";
 import favoriteIcon from "../icons/favoriteIcon.svg";
 import favoriteFillIcon from "../icons/favoriteFillIcon.svg";
 import readedIcon from "../icons/readedIcon.svg";
+import readedIconFill from "../icons/readedIconFill.svg";
 import reviewIcon from "../icons/reviewIcon.svg";
 import CardMenu from "./CardMenu.css";
 import {
@@ -21,6 +31,9 @@ import {
 
 export default function Card({ id, cover, modal, setModal }) {
   const [open, setOpen] = useState(false);
+  const [favorite,setFavorite]=useState(false)
+  const [readed,setReaded]=useState(false)
+  const [reading,setReading]=useState(false)
 
   function handleClick(e) {
     e.preventDefault(e);
@@ -30,6 +43,44 @@ export default function Card({ id, cover, modal, setModal }) {
 
   const dispatch = useDispatch();
   const book = useSelector((state) => state.detail);
+
+  function handleFavorite(id) {
+    // e.preventDefault();
+    // console.log("e.target.value",e.target.value)
+    console.log("Entré a favorite:", id);
+    if(!favorite){
+      setFavorite(!favorite)
+      console.log("FAV+",favorite)
+      dispatch(addFavorite(id));
+      
+    }
+    if(favorite){
+      setFavorite(!favorite)
+      console.log("FAV-",favorite)
+      dispatch(deleteFavorite(id));
+      
+    }
+    
+  }
+  
+  function handleReaded(id) {
+    
+    // console.log("e.target.value",e.target.value)
+    console.log("Entré a readed:", id);
+    if(!readed){
+      setReaded(!readed)
+      console.log("READ+",readed)
+      dispatch(addReaded(id));
+      
+    }
+    if(readed){
+      setReaded(!readed)
+      console.log("READ-",readed)
+      dispatch(deleteReaded(id));
+      
+    }
+    
+  }
 
   return (
     <>
@@ -52,9 +103,9 @@ export default function Card({ id, cover, modal, setModal }) {
             className={`dropdown-menu ${open ? "active" : "inactive"}`}
           >
             <UlCard>
-              <DropdownItem icon={reviewIcon}/>
-              <DropdownItem icon={readedIcon}/>
-              <DropdownItem icon={favoriteIcon}/>
+              <DropdownItem icon={reviewIcon} value={id} role="button" />
+              <DropdownItem icon={!readed?readedIcon:readedIconFill} value={id} handle={e=>{handleReaded(id)}} role="button"/>
+              <DropdownItem icon={!favorite ? favoriteIcon:favoriteFillIcon} value={id} handle={e=>{handleFavorite(id)}} role="button"/>
             </UlCard>
           </DropDownMenu>
         </MenuConteiner>
@@ -67,7 +118,7 @@ export default function Card({ id, cover, modal, setModal }) {
 function DropdownItem(props) {
   return (
     <li>
-      <img src={props.icon} alt="n" />
+      <img src={props.icon} alt="n" role="button" onClick={props.handle} value={props.id}/>
     </li>
   );
 }
--- src/components/CardDetail.jsx
+++ src/components/CardDetail.jsx
@@ -1,7 +1,6 @@
 import React,{useState} from "react";
 import { useDispatch } from "react-redux";
 
-import { cleanDetail } from "../redux/actions";
 import {
   CardImgDetail,
   ImgContainerDetail,
@@ -33,6 +32,7 @@ import {
   DropDownMenu,
 } from "../styles/Card";
 import {
+  cleanDetail,
   addFavorite,
   addReaded,
   addReading,
@@ -56,7 +56,7 @@ import {StarsContainer} from "../styles/CardRecomended"
 function DropdownItem(props) {
   return (
     <li>
-      <img src={props.icon} alt="n" />
+      <img src={props.icon} alt="n" role="button" onClick={props.handle} value={props.id}/>
     </li>
   );
 }
@@ -71,7 +71,7 @@ export default function CardDetail({book,modal,setModal}) {
   const [favorite,setFavorite]=useState(false)
   const [readed,setReaded]=useState(false)
   const [reading,setReading]=useState(false)
-  
+  console.log("BOOKID",book.id)
   // const bookId = props.match.params.id;
   // console.log("BOOK ID:", bookId);
   // console.log("PROPS",props)
@@ -92,37 +92,44 @@ export default function CardDetail({book,modal,setModal}) {
   function handleCloseClick(e) {
     e.preventDefault(e);
     setModal(false);
-    dispatch(cleanDetail());
+    console.log("e.target.value",e.target.value)
+    dispatch(cleanDetail(e.target.value));
   }
  
-  function handleFavorite(e) {
-    e.preventDefault();
-    console.log("Entré a favorite:", e);
+  function handleFavorite(id) {
+    // e.preventDefault();
+    // console.log("e.target.value",e.target.value)
+    console.log("Entré a favorite:", id);
     if(!favorite){
-      dispatch(addFavorite());
       setFavorite(!favorite)
-      console.log("FAV1",favorite)
+      console.log("FAV+",favorite)
+      dispatch(addFavorite(id));
+      
     }
     if(favorite){
-      dispatch(deleteFavorite());
       setFavorite(!favorite)
-      console.log("FAV2",favorite)
+      console.log("FAV-",favorite)
+      dispatch(deleteFavorite(id));
+      
     }
     
   }
   
-  function handleReaded(e) {
-    e.preventDefault();
-    console.log("Entré a readed:", e);
+  function handleReaded(id) {
+    // e.preventDefault();
+    // console.log("e.target.value",e.target.value)
+    console.log("Entré a readed:", id);
     if(!readed){
-      dispatch(addReaded());
       setReaded(!readed)
-      console.log("READ1",readed)
+      console.log("READ+",readed)
+      dispatch(addReaded(id));
+     
     }
     if(readed){
-      dispatch(deleteReaded());
       setReaded(!readed)
-      console.log("READ2",readed)
+      console.log("READ-",readed)
+      dispatch(deleteReaded(id));
+      
     }
     
   }
@@ -182,9 +189,9 @@ var review2Star=book && book.reviews && book.reviews[1] && starRating(book.revie
             className={`dropdown-menu ${open ? "active" : "inactive"}`}
           >
             <UlCard>
-              <DropdownItem icon={reviewIcon}/>
-              <DropdownItem icon={readedIcon} onClick={e=>{handleReaded(e)}}/>
-              <DropdownItem icon={favoriteIcon} onClick={e=>{handleFavorite(e)}}/>
+              <DropdownItem icon={reviewIcon} value={book.id}/>
+              <DropdownItem icon={!readed?readedIcon:readedIconFill} value={book.id} handle={e=>{handleReaded(book.id)}} role="button"/>
+              <DropdownItem icon={!favorite ? favoriteIcon:favoriteFillIcon} value={book.id} handle={e=>{handleFavorite(book.id)}} role="button"/>
             </UlCard>
           </DropDownMenu>
         </MenuConteiner>
@@ -260,12 +267,8 @@ var review2Star=book && book.reviews && book.reviews[1] && starRating(book.revie
             <StarsContainer>
           {
             review2Star && review2Star.map(s=>(s==="star"?
-            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-star-fill" viewBox="0 0 16 16">
-              <path d="M3.612 15.443c-.386.198-.824-.149-.746-.592l.83-4.73L.173 6.765c-.329-.314-.158-.888.283-.95l4.898-.696L7.538.792c.197-.39.73-.39.927 0l2.184 4.327 4.898.696c.441.062.612.636.282.95l-3.522 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256z"/>
-            </svg>:
-            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-star-half" viewBox="0 0 16 16">
-                <path d="M5.354 5.119 7.538.792A.516.516 0 0 1 8 .5c.183 0 .366.097.465.292l2.184 4.327 4.898.696A.537.537 0 0 1 16 6.32a.548.548 0 0 1-.17.445l-3.523 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256a.52.52 0 0 1-.146.05c-.342.06-.668-.254-.6-.642l.83-4.73L.173 6.765a.55.55 0 0 1-.172-.403.58.58 0 0 1 .085-.302.513.513 0 0 1 .37-.245l4.898-.696zM8 12.027a.5.5 0 0 1 .232.056l3.686 1.894-.694-3.957a.565.565 0 0 1 .162-.505l2.907-2.77-4.052-.576a.525.525 0 0 1-.393-.288L8.001 2.223 8 2.226v9.8z"/>
-              </svg>
+            <StarDetail src={starFill} alt="n" />:
+            <StarDetail src={starHalf} alt="n" />
             ))
             } 
           </StarsContainer>
