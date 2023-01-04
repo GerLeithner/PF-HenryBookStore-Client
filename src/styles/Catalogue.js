6dc3eac48971180ff4d0d5a304bbcd209eef0096
Adrianl93 <[200~laborde.adrian2012@gmail.comgit config --global user.email laborde.adrian2012@gmail.com>
2023-01-04 18:17:59 -0300

    landing remake

--- src/styles/Catalogue.js
+++ src/styles/Catalogue.js
@@ -33,11 +33,12 @@ border: none;
 
 export const ButtonCatalogue = styled.button`
   width: ${({ ancho }) => ancho || "max-content"};
-  padding: 1em 1em;
-  font-size: 12px;
+  height: ${({ alto }) => alto || "max-content"};
+  padding: ${({ padding }) => padding || "1em 1em"};
+  font-size: ${({ fontSize }) => fontSize || "12px"};
   text-transform: uppercase;
   letter-spacing: 2.5px;
-  font-weight: 200;
+  font-weight: ${({ fontWeight }) => fontWeight || "200"};
   color: #111;
   background-color: #fff;
   border: none;
--- src/styles/Landing.js
+++ src/styles/Landing.js
@@ -1,13 +1,25 @@
 import styled from "styled-components";
-
+export const BackgroundConteiner = styled.div`
+     width: 100vw;
+     height: 100vh;
+     position: fixed;
+     top: 0;
+     left: 0;
+     display: flex;
+     align-items: center;
+     justify-content: center;
+`;
 
 export const BoxContainer = styled.div`
      display: flex;
      flex-direction: column;
-     width: 500px; 
-     height: 500px; 
-     border: 3px solid black, 1px solid whitesmoke;
-     border-style:  inset;
+     width: 550px; 
+     height: 550px; 
+     border: 3px solid black;
+     position: fixed;
+     display: flex;
+     align-items: center;
+     justify-content: center;
 `;
 
 export const ButtonsConteiner = styled.div`
@@ -26,4 +38,25 @@ export const H1Landing = styled.h1`
      
 
    
+`;
+export const H4Landing = styled.h4`
+     padding: 1px;
+     margin: 1px;
+
+   
+`;
+
+export const PromotionalConteiner = styled.div`
+display: flex;
+flex-direction: column;
+height: max-content;
+align-items: center;
+justify-content: center;
+padding: 10px;
+margin: 10px;
+margin-bottom: 0px;
+
+
+
+
 `;
